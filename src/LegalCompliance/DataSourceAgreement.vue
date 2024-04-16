a150f9dd5b5575421372e0aa73bed441270f73dc
Miro Benicio <mirodantas27@gmail.com>
2024-04-16 11:48:24 -0300

    feat: data source agreements

--- src/LegalCompliance/DataSourceAgreement.vue
+++ src/LegalCompliance/DataSourceAgreement.vue
@@ -1,12 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const datasets = [1, 2, 3]
+</script>
 
 <template>
   <div>
     <h1>DATA SOURCE AGREEMENTS</h1>
-    <div class="agreement-checkbox-container">
-      <input type="checkbox" id="cb1" />
-      <label for="cb1"></label>
-      <span>3,103,398 RECORDS</span>
+    <div class="agreement-checkbox-container" v-for="dataset in datasets" :key="dataset">
+      <input type="checkbox" :id="`${dataset}`" @click="console.log('test')" />
+      <label :for="`${dataset}`"></label>
+      <div class="odd-div">3,103,398 RECORDS</div>
       <span>APP 1</span>
     </div>
   </div>
@@ -20,32 +22,42 @@ h1 {
   font-weight: 600;
 }
 
-input[id='cb1'] + label {
-  display: inline-block;
+input[type='checkbox'] + label {
+  display: block;
   width: 1.2em;
   height: 1.2em;
   border: 1px solid var(--accent-color);
   border-radius: 0.25em;
   cursor: pointer;
 }
-input[id='cb1']:checked + label:after {
-  position: relative;
+input[type='checkbox']:checked + label:after {
   display: flex;
   align-items: center;
   justify-content: center;
   content: '\2713';
   font-size: 0.85em;
 }
-input[id='cb1'] {
+
+input[type='checkbox'] {
   display: none;
 }
 
+.odd-div {
+  display: block;
+  border-left: 1px solid #00000020;
+  border-right: 1px solid #00000020;
+  padding: 0.5em 1em;
+}
 .agreement-checkbox-container {
   display: flex;
   align-items: center;
-  gap: 2em;
+  justify-content: space-evenly;
   border: 1px solid #00000020;
-  padding: 0.5em 1em;
   border-radius: 0.5em;
+  margin-bottom: 1em;
+}
+
+.agreement-checkbox-container span {
+  text-decoration: underline;
 }
 </style>
