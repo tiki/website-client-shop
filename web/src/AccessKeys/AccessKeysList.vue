44bfaf98aa92f0da8aeead8436b109e64a078656
Miro Benicio <mirodantas27@gmail.com>
2024-05-30 14:12:35 -0300

    access keys screen

--- web/src/AccessKeys/AccessKeysList.vue
+++ web/src/AccessKeys/AccessKeysList.vue
@@ -1,87 +1,115 @@
 <script setup lang="ts">
-
+import { reactive, ref } from 'vue'
 
 const key = '******8f028'
+const labels = ['Key', 'Label', 'Created', 'Last Used', '']
+
+const keys = reactive([
+  {
+    hashedKey: '******8f028',
+    key: 'a8y901m8f028',
+    keyLabel: 'OAS Upload',
+    created: '7 months ago',
+    lastUse: '6 months agor',
+    isHidden: true
+  },
+  {
+    hashedKey: '******8f028',
+    key: 'a8y901m8f028',
+    keyLabel: 'OAS Upload',
+    created: '7 months ago',
+    lastUse: '6 months ago',
+    isHidden: true
+  }
+])
+
+const toggleVisibility = (index: number) => {
+  keys[index].isHidden = !keys[index].isHidden
+}
+
+const copyToClipboard = (value: string) => {
+  navigator.clipboard.writeText(value)
+}
 </script>
 
 <template>
-  <!-- <div class="list-container">
-        <div class="list-header">
-            <p>Key</p>
-            <p>Label</p>
-            <p>Created</p>
-            <p>Last Used</p>
-            <p ></p>
+  <div class="list-header">
+    <h2>Api Keys</h2>
+    <button>
+        + Add Key
+    </button>
+  </div>
+  <div class="grid">
+    <span v-for="label in labels" :key="label" class="labels">
+      {{ label }}
+    </span> 
+    <template v-for="(keyItem, index) in keys" :key="index">
+      <span>
+        <div class="key-container">
+          <p>{{ keyItem.isHidden ? keyItem.hashedKey : keyItem.key }}</p>
+          <button @click="toggleVisibility(index)">
+            <img src="../assets/visibility_on.svg" alt="" v-if="keyItem.isHidden" />
+            <img src="../assets/visibility_off.svg" alt="" v-if="!keyItem.isHidden" />
+          </button>
+          <button @click="copyToClipboard(keyItem.key)">
+            <img src="../assets/copy.svg" alt="" />
+          </button>
         </div>
-        <div class="list-body">
-            <p>Key</p>
-            <p>OAS Upload</p>
-            <p>7 months ago</p>
-            <p>6 months ago</p>
-            <p></p>
-        </div>
-    </div> -->
-  <table class="list-container">
-    <thead>
-      <tr class="list-header">
-        <th>Key</th>
-        <th>Label</th>
-        <th>Created</th>
-        <th>Last Used</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr class="list-body">
-        <td>{{ key}}</td>
-        <td>OAS Utdload</td>
-        <td>7 months ago</td>
-        <td>6 months ago</td>
-        <td>
-            <button>
-                <img src="../assets/three_dots_icons.svg" alt="">
-            </button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+      </span>
+      <span>{{ keyItem.keyLabel }}</span>
+      <span>{{ keyItem.created }}</span>
+      <span>{{ keyItem.lastUse }}</span>
+      <span class="options-button">
+        <button>
+          <img src="../assets/three_dots_icons.svg" alt="" />
+        </button>
+      </span>
+    </template>
+  </div>
 </template>
 
 <style scoped>
+button {
+  border: none;
+  background: transparent;
+  cursor: pointer;
+}
 
-.list-container {
+.options-button{
+    text-align: right;
+}
+
+.labels {
+  text-transform: uppercase;
+  font-weight: 500;
+}
+.key-container {
   display: flex;
-  flex-direction: column;
-  width: 100%;
   gap: 1em;
 }
 
-
-button{
-    border: none;
-    background: transparent;
-    cursor: pointer;
+.grid {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
 }
 
-tbody {
-  display: flex;
-  flex-direction: column;
+.grid > span {
+  padding: 8px 4px;
 }
 
-.list-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 0 1em 0;
-  width: 100%;
-  border-bottom: 1px solid #00000010;
-  font-weight: 500;
+.list-header{
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1em;
+    align-items: center;
 }
 
-.list-body {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 1em 0 0;
+.list-header button { 
+    font-size: 1.2em;
+    background: var(--accent-color);
+    padding: 0.35em 0.5em;
+    border: 1px solid var(--accent-color);
+    border-radius: 0.25em;
+    color: white;
 }
 </style>
